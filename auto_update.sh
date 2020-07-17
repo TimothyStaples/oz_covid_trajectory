f0ef78f6abe4440a0ee5624a2c622f1f4286f079
Timothy Staples <timothy.staples@uqconnect.edu.au>
2020-07-17 11:02:23 +1000

    auto-commit from on Fri 17 Jul 2020 11:01:46 AEST

--- auto_update.sh
+++ auto_update.sh
@@ -2,6 +2,13 @@ R
 source("oz_covid_analysis.R")
 quit("no")
 
+git config --global user.name "Timothy Staples"
+git config --global user.email "timothy.staples@uqconnect.edu.au"
 
 git commit -m date
 git push
+
+
+
+git commit -F /tmp/Rtmpqz7of9/git-commit-message-1ebc5cfd8376.txt
+git push origin HEAD:refs/heads/master
